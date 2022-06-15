a507df91a11c30108573faee79ceb4196ecb5d04
Lucas Rodrigues <rodrigges@Lucass-iMac.local>
2022-06-15 13:16:10 -0300

    :lipstick: updating css: buttons, inputs

--- calculator/code.js
+++ calculator/code.js
@@ -4,14 +4,14 @@ const QSA = (pam) => document.querySelectorAll(pam);
 const operation_display = (e) => {
   const TEXT = e.target.innerText;
   const OPERATION_DISPLAY = QS('#operation-display');
-  
+
   OPERATION_DISPLAY.value += TEXT;
 }
 
 const calculate = () => {
   const INPUT_DISPLAY = QS('#input-display');
   const OPERATION_DISPLAY = QS('#operation-display');
-  
+
   INPUT_DISPLAY.value = eval(OPERATION_DISPLAY.value);
 }
 
@@ -21,21 +21,21 @@ const check_calc = (e) => {
   let str = OPERATION_DISPLAY.value;
 
   if (OPERATION_DISPLAY.value === '') INPUT_DISPLAY.value = '';
-  
-  if((str[0] === '/' || str[0] === '*' )) {
+
+  if ((str[0] === '/' || str[0] === '*')) {
     OPERATION_DISPLAY.value = '';
-    return alert ('Operação inválida!');
+    return alert('Operação inválida!');
   }
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === '+' || str[i] === '-' || str[i] === '/' || str[i] === '*' ) {
+    if (str[i] === '+' || str[i] === '-' || str[i] === '/' || str[i] === '*') {
       if (str[i + 1]) {
-        if (str[i+1] === '+' || str[i+1] === '-' || str[i+1] === '/' || str[i+1] === '*' ) {
+        if (str[i + 1] === '+' || str[i + 1] === '-' || str[i + 1] === '/' || str[i + 1] === '*') {
           OPERATION_DISPLAY.value = '';
-          return alert ('Operação inválida!');
+          return alert('Operação inválida!');
         }
-              
-      } 
+
+      }
     }
   }
 }
@@ -43,7 +43,7 @@ const check_calc = (e) => {
 const ac_calculator = () => {
   const INPUT_DISPLAY = QS('#input-display');
   const OPERATION_DISPLAY = QS('#operation-display');
-  
+
   INPUT_DISPLAY.value = '';
   OPERATION_DISPLAY.value = '';
 }
@@ -55,7 +55,7 @@ window.addEventListener('load', () => {
   // const INPUT_DISPLAY = QS('#input-display');
   const EVERY_BTNS = QSA('button');
   const OPERATION_DISPLAY = QS('#operation-display');
-  
+
   BTN_NUMBERS.forEach(btn => {
     btn.addEventListener('click', operation_display);
     btn.addEventListener('click', calculate);
